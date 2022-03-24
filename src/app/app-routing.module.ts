754f9f6cd13909db0a3816dfc9a147643c4693cf
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2022-03-24 13:44:13 +0200

    CMSF-6-fd-feat-prescription-service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   // { path: 'patients' },
   // { path: 'reciptionists' },
   // { path: 'mediciens' },
+  { path: 'prescriptions', component: ViewTableComponent },
   { path: 'clinics', component: ViewTableComponent },
   // { path: '**' },
 ];
--- src/app/view-table/view-table.component.ts
+++ src/app/view-table/view-table.component.ts
@@ -2,35 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Clinic } from '../model/clinic';
 import { Doctor } from '../model/doctor';
+import { Prescription } from '../model/prescription';
 import { ClinicService } from '../service/clinic.service';
 import { DoctorService } from '../service/doctor.service';
+import { PrescriptionService } from '../service/prescription.service';
 
 @Component({
   selector: 'app-view-table',
   templateUrl: './view-table.component.html',
-  styleUrls: ['./view-table.component.scss']
+  styleUrls: ['./view-table.component.scss'],
 })
 export class ViewTableComponent implements OnInit {
   ELEMENT_DATA: {}[] = [{}];
 
-  constructor(private doctorServ: DoctorService, private clincServ: ClinicService, private router: Router) {
-   
-   }
+  constructor(
+    private doctorServ: DoctorService,
+    private clincServ: ClinicService,
+    private prescriptionServ: PrescriptionService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     switch (this.router.url) {
       case '/doctors':
-        this.doctorServ.getAll().subscribe(((data: Doctor[]) => {
+        this.doctorServ.getAll().subscribe((data: Doctor[]) => {
           console.log(data);
           this.ELEMENT_DATA = data;
-        }));
+        });
         break;
 
       case '/clinics':
-        this.clincServ.getAll().subscribe(((data: Clinic[]) => {
+        this.clincServ.getAll().subscribe((data: Clinic[]) => {
           console.log(data);
           this.ELEMENT_DATA = data;
-        }));
+        });
         break;
 
       case '/appointments':
@@ -47,12 +52,16 @@ export class ViewTableComponent implements OnInit {
 
       case '/prescriptions':
         // call service
+        this.prescriptionServ.getAll().subscribe((data: Prescription[]) => {
+          console.log(data);
+          this.ELEMENT_DATA = data;
+        });
         break;
 
       case '/receptionists':
         // call service
         break;
-    
+
       default:
         break;
     }
