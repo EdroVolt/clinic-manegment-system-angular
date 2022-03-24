9778c60d2ce14e14d3e663fca83c3d0ad50b3671
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2022-03-25 01:28:14 +0200

    fd-feat: Receptionist service

--- src/app/service/receptionist.service.ts
+++ src/app/service/receptionist.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Receptionist } from './../model/receptionist';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReceptionistService {
+  receptionist: Receptionist[] = [];
+  baseURL = 'http://localhost:8080/receptionist/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getAll() {
+    return this.http.get<Receptionist[]>(this.baseURL);
+  }
+  getOne(_id: string) {
+    return this.http.get<Receptionist>(this.baseURL + _id);
+  }
+  add(newReceptionist: Receptionist) {
+    return this.http.post<Receptionist>(this.baseURL, {
+      receptionist: newReceptionist,
+    });
+  }
+  edit(updatedReceptionist: Receptionist) {
+    return this.http.put<Receptionist>(this.baseURL + updatedReceptionist._id, {
+      receptionist: updatedReceptionist,
+    });
+  }
+  delete(_id: string) {
+    return this.http.delete<Receptionist>(this.baseURL + _id);
+  }
 }
