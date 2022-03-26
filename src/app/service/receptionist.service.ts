e0b0df1918077263cdd512bdb4515970476065b3
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-03-26 19:41:21 +0200

    bd-fix: add & delete request body

--- src/app/service/receptionist.service.ts
+++ src/app/service/receptionist.service.ts
@@ -16,14 +16,10 @@ export class ReceptionistService {
     return this.http.get<Receptionist>(this.baseURL + _id);
   }
   add(newReceptionist: Receptionist) {
-    return this.http.post<Receptionist>(this.baseURL, {
-      receptionist: newReceptionist,
-    });
+    return this.http.post<Receptionist>(this.baseURL, { ...newReceptionist});
   }
   edit(updatedReceptionist: Receptionist) {
-    return this.http.put<Receptionist>(this.baseURL + updatedReceptionist._id, {
-      receptionist: updatedReceptionist,
-    });
+    return this.http.put<Receptionist>(this.baseURL + updatedReceptionist._id, { ...updatedReceptionist });
   }
   delete(_id: string) {
     return this.http.delete<Receptionist>(this.baseURL + _id);
